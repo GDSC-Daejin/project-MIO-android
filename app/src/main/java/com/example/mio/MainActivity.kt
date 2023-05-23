c1072db7d2860d4f2226f72d87ce90c93e040fd4
sonms <smsms5676@naver.com>
2023-05-23 22:48:08 +0900

    feature/AND/바텀 네비게이션 메뉴 바 및 탭뷰, 로그인, 메인 액티비티 기능별 세팅(메뉴 변경 가능)

--- app/src/main/java/com/example/mio/MainActivity.kt
+++ app/src/main/java/com/example/mio/MainActivity.kt
@@ -1,176 +1,115 @@
 package com.example.mio
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mio.Model.LoginGoogleResponse
-import com.example.mio.NoticeBoard.NoticeBoardActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.mio.Navigation.AccountFragment
+import com.example.mio.Navigation.BlankFragment
+import com.example.mio.Navigation.HomeFragment
+import com.example.mio.Navigation.SearchFragment
 import com.example.mio.databinding.ActivityMainBinding
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import okhttp3.*
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mGoogleSignInClient: GoogleSignInClient
-    lateinit var resultLauncher: ActivityResultLauncher<Intent>
-    lateinit var mBinding : ActivityMainBinding
-    private val CLIENT_WEB_ID_KEY = BuildConfig.client_web_id_key
-    private val CLIENT_WEB_SECRET_KEY = BuildConfig.client_web_secret_key
-    private val test = "test"
+    private lateinit var mBinding : ActivityMainBinding
+    private val TAG_HOME = "home_fragment"
+    private val TAG_SEARCH = "search_fragment"
+    private val TAG_ACCOUNT = "account_fragment"
+    private val TAG_CAMERA = "CAMERA_fragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        setResultSignUp()
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            //.requestIdToken(R.string.defalut_client_id.toString())
-            .requestServerAuthCode(CLIENT_WEB_ID_KEY)
-            .requestProfile()
-            .build()
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        mBinding.signInButton.setOnClickListener {
-            signIn()
-        }
-
-        mBinding.textView.setOnClickListener {
-            val intent = Intent(this, NoticeBoardActivity::class.java).apply {
-                //putExtra("type", "")
-            }
-            startActivity(intent)
-        }
-
-    }
-    private fun setResultSignUp() {
-        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleSignInResult(task)
-                Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
-            }
-        }
+        setFragment(TAG_HOME, HomeFragment())
+        initNavigationBar()
     }
 
+    private fun initNavigationBar() {
+        mBinding.bottomNavigationView.
+            setOnItemSelectedListener {item ->
+                when(item.itemId) {
+                    R.id.navigation_home ->
+                        setFragment(TAG_HOME, HomeFragment())
 
-    private fun handleSignInResult(completedTask : Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            val email = account?.email.toString()
-            val authCode = account.serverAuthCode
-            val saveSharedPreferenceGoogleLogin = SaveSharedPreferenceGoogleLogin()
+                    R.id.navigation_search ->
+                        setFragment(TAG_SEARCH, SearchFragment())
 
-            //회원가입과 함께 새로운 계정 정보 저장
-            if (saveSharedPreferenceGoogleLogin.getUserEMAIL(this@MainActivity)!!.isEmpty()) {
-                //나중에 재개편 필요함 -> navigation graph를 정리할 필요성이 있음
-                // call Login Activity
-                /*val intent = Intent(this@MainActivity, LoginActivity::class.java)
-                intent.putExtra("email", saveSharedPreferenceGoogleLogin.setUserEMAIL(this, email).toString())
-                startActivity(intent)
-                finish()*/
+                    R.id.navigation_camera ->
+                        setFragment(TAG_CAMERA, BlankFragment())
 
-            } else { //현재 로그인, 또는 로그인했던 정보가 저장되어있으면 home으로
-                val intent = Intent(this, NoticeBoardActivity::class.java)
-                startActivity(intent)
-                this.finish()
-            }
+                    R.id.navigation_account ->
+                        setFragment(TAG_ACCOUNT, AccountFragment())
 
-            Toast.makeText(this, "tjd", Toast.LENGTH_SHORT).show()
-            println(email)
-            println(authCode.toString())
 
-            getAccessToken(authCode!!)
+                    else ->
+                        setFragment(TAG_HOME, HomeFragment())
 
-        } catch (e : ApiException) {
-            Log.w("failed", "signinresultfalied code = " + e.statusCode)
-        }
-    }
-
-    private fun getAccessToken(authCode : String) {
-        val client = OkHttpClient()
-        val requestBody: RequestBody = FormBody.Builder()
-            //1시간
-            .add("grant_type", "authorization_code")
-            .add(
-                "client_id",
-                CLIENT_WEB_ID_KEY
-            )
-            .add("client_secret", CLIENT_WEB_SECRET_KEY)
-            .add("redirect_uri", "")
-            .add("code", authCode)
-            //refresh token 필요시
-            .add("response_type", "code")
-            .add("access_type", "offline")
-            .add("approval_prompt", "force")
-            .build()
-
-        val request = Request.Builder()
-            .url("https://oauth2.googleapis.com/token")
-            .post(requestBody)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                print("Failed")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                try {
-                    val jsonObject = JSONObject(response.body!!.string())
-                    val message = jsonObject.keys() //.toString(5)
-
-                    //json파일 키와 벨류를 잠시담는 변수
-                    val tempKey = ArrayList<String>()
-                    val tempValue = ArrayList<String>()
-
-                    val user_info = ArrayList<LoginGoogleResponse>()
-                    user_info.clear()
-                    while (message.hasNext()) {
-                        val s = message.next().toString()
-                        tempKey.add(s)
-
-                    }
-
-                    for (i in tempKey.indices) {
-                        //fruitValueList.add(fruitObject.getString(fruitKeyList.get(j)));
-                        tempValue.add(jsonObject.getString(tempKey[i]))
-                        println(tempKey[i] + "/" + jsonObject.getString(tempKey[i]))
-                    }
-
-                    user_info.add(LoginGoogleResponse(tempValue[0], tempValue[1].toInt(), tempValue[2], tempValue[3], tempValue[4]))
-                    println()
-                    println(message)
-                    println(user_info[0].id_token)
-                    tempKey.clear()
-                    tempValue.clear()
-                } catch (e: JSONException) {
-                    e.printStackTrace()
                 }
+                true
             }
 
-        })
-    }
-    private fun signIn() {
-        val signIntent = mGoogleSignInClient.signInIntent
-        resultLauncher.launch(signIntent)
+
     }
 
 
+    fun setFragment(tag : String, fragment: Fragment) {
+        val manager : FragmentManager = supportFragmentManager
+        val bt = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null) {
+            bt.add(R.id.fragment_content, fragment, tag)
+        }
+
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val search = manager.findFragmentByTag(TAG_SEARCH)
+        val camera = manager.findFragmentByTag(TAG_CAMERA)
+        val account = manager.findFragmentByTag(TAG_ACCOUNT)
+
+
+        if (home != null) {
+            bt.hide(home)
+        }
+        if (search != null) {
+            bt.hide(search)
+        }
+        if (camera != null) {
+            bt.hide(camera)
+        }
+        if (account != null) {
+            bt.hide(account)
+        }
+
+        if (tag == TAG_HOME) {
+            if (home != null) {
+                bt.show(home)
+            }
+        }
+        else if (tag == TAG_SEARCH) {
+            if (search != null) {
+                bt.show(search)
+            }
+        }
+        else if (tag == TAG_CAMERA) {
+            if (camera != null) {
+                bt.show(camera)
+            }
+        }
+        else if (tag == TAG_ACCOUNT) {
+            if (account != null) {
+                bt.show(account)
+            }
+        }
+        bt.commitAllowingStateLoss()
+    }
+
+    fun changeFragment(fragment : Fragment) {
+        //프래그먼트를 교체 하는 작업을 수행할 수 있게 해줍니다.
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_content, fragment)
+            .commit()
+    }
 }

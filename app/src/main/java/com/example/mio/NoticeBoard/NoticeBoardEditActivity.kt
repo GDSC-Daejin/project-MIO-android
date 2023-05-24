a158ba0a2e5a7bcb1c79a8716d9322df3a5a06e0
sonms <smsms5676@naver.com>
2023-05-24 23:49:51 +0900

    feature/AND/탭 레이아웃으로 카테고리 기능과 게시판 보기 기능과 글쓰기 기능 구현

--- app/src/main/java/com/example/mio/NoticeBoard/NoticeBoardEditActivity.kt
+++ app/src/main/java/com/example/mio/NoticeBoard/NoticeBoardEditActivity.kt
@@ -1,18 +1,52 @@
 package com.example.mio.NoticeBoard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mio.Model.PostData
 import com.example.mio.R
 import com.example.mio.databinding.ActivityNoticeBoardEditBinding
 
 class NoticeBoardEditActivity : AppCompatActivity() {
     private lateinit var mBinding : ActivityNoticeBoardEditBinding
-
+    //클릭한 포스트(게시글)의 데이터 임시저장 edit용
+    private var temp : PostData? = null
+    private var pos = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityNoticeBoardEditBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        val type = intent.getStringExtra("type")
 
+        if (type.equals("ADD")) { //add
+            //temp = intent.getSerializableExtra("postItem") as PostData?
+            /*mBinding.editAdd.text = temp!!.postContent
+            nbrBinding.readAccountId.text = temp!!.accountID*/
+        } else { //edit
+
+        }
+
+        mBinding.editAdd.setOnClickListener {
+            val contentPost = mBinding.editPostContent.text.toString()
+            val contentTitle = mBinding.editPostTitle.text.toString()
+
+            if (type.equals("ADD")) {
+                if (contentPost.isNotEmpty() && contentTitle.isNotEmpty()) {
+                    //데이터 세팅 후 임시저장
+                    val tempData = PostData("accoout", pos, contentTitle, contentPost)
+
+                    val intent = Intent().apply {
+                        putExtra("postData", tempData)
+                        putExtra("flag", 0)
+                    }
+                    pos += 1
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+            } else {
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/mio/TabCategory/TaxiTabFragment.kt
@@ -0,0 +1,240 @@
+package com.example.mio.TabCategory
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mio.Adapter.NoticeBoardAdapter
+import com.example.mio.Model.PostData
+import com.example.mio.NoticeBoard.NoticeBoardEditActivity
+import com.example.mio.NoticeBoard.NoticeBoardReadActivity
+import com.example.mio.R
+import com.example.mio.databinding.FragmentHomeBinding
+import com.example.mio.databinding.FragmentTaxiTabBinding
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TaxiTabFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TaxiTabFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var taxiTabBinding: FragmentTaxiTabBinding
+    private var manager : LinearLayoutManager = LinearLayoutManager(activity)
+    private var noticeBoardAdapter : NoticeBoardAdapter? = null
+    //게시글 데이터
+    private var data : MutableList<PostData?> = mutableListOf()
+    var dataPosition = 0
+    //게시글 포지션
+    private var position = 0
+    //뒤로 가기 받아오기
+    private lateinit var callback : OnBackPressedCallback
+    var backPressedTime : Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        taxiTabBinding = FragmentTaxiTabBinding.inflate(inflater, container, false)
+
+        initRecyclerView()
+
+        //recyclerview item클릭 시
+        noticeBoardAdapter!!.setItemClickListener(object : NoticeBoardAdapter.ItemClickListener {
+            override fun onClick(view: View, position: Int, itemId: Int) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val temp = data[position]
+                    dataPosition = position
+                    val intent = Intent(activity, NoticeBoardReadActivity::class.java).apply {
+                        putExtra("type", "READ")
+                        putExtra("postItem", temp)
+                    }
+                    requestActivity.launch(intent)
+                }
+            }
+        })
+
+        //여기서 edit으로 이동동
+        taxiTabBinding.addBtn.setOnClickListener {
+            /*data.add(PostData("2020202", 0, "test", "test"))
+            noticeBoardAdapter!!.notifyItemInserted(position)
+            position += 1*/
+            val intent = Intent(activity, NoticeBoardEditActivity::class.java).apply {
+                putExtra("type","ADD")
+            }
+            requestActivity.launch(intent)
+            //noticeBoardAdapter!!.notifyDataSetChanged()
+        }
+
+
+        return taxiTabBinding.root
+    }
+
+    private fun initRecyclerView() {
+        noticeBoardAdapter = NoticeBoardAdapter()
+        noticeBoardAdapter!!.postItemData = data
+        taxiTabBinding.noticeBoardRV.adapter = noticeBoardAdapter
+        //레이아웃 뒤집기 안씀
+        //manager.reverseLayout = true
+        //manager.stackFromEnd = true
+        taxiTabBinding.noticeBoardRV.setHasFixedSize(true)
+        taxiTabBinding.noticeBoardRV.layoutManager = manager
+
+        taxiTabBinding.noticeBoardRV.itemAnimator =  SlideInUpAnimator(OvershootInterpolator(1f))
+        taxiTabBinding.noticeBoardRV.itemAnimator?.apply {
+            addDuration = 1000
+            removeDuration = 100
+            moveDuration = 1000
+            changeDuration = 100
+        }
+
+    }
+
+    private val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { it ->
+        when (it.resultCode) {
+            AppCompatActivity.RESULT_OK -> {
+                val post = it.data?.getSerializableExtra("post") as PostData
+                when(it.data?.getIntExtra("flag", -1)) {
+                    //add
+                    0 -> {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            data.add(post)
+                        }
+                        noticeBoardAdapter!!.notifyItemInserted(post.postID)
+                    }
+                    //edit
+                    1 -> {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            data[dataPosition] = post
+                        }
+                        noticeBoardAdapter!!.notifyItemChanged(post.postID)
+                    }
+
+                }
+                //getSerializableExtra = intent의 값을 보내고 받을때사용
+                //타입 변경을 해주지 않으면 Serializable객체로 만들어지니 as로 캐스팅해주자
+                /*val pill = it.data?.getSerializableExtra("pill") as PillData
+                val selectCategory = it.data?.getSerializableExtra("cg") as String*/
+
+                //선택한 카테고리 및 데이터 추가
+
+
+                /*if (selectCategory.isNotEmpty()) {
+                    selectCategoryData[selectCategory] = categoryArr
+                }*/
+
+
+                //api 33이후 아래로 변경됨
+                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    getSerializable(key, T::class.java)
+                } else {
+                    getSerializable(key) as? T
+                }*/
+                /*when(it.data?.getIntExtra("flag", -1)) {
+                    //add
+                    0 -> {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            data.add(pill)
+                            categoryArr.add(pill)
+                            //add면 그냥 추가
+                            selectCategoryData[selectCategory] = categoryArr
+                            //전
+                            //println( categoryArr[dataPosition])
+                        }
+                        println("전 ${selectCategoryData[selectCategory]}")
+                        //livedata
+                        sharedViewModel!!.setCategoryLiveData("add", selectCategoryData)
+
+
+                        homeAdapter!!.notifyDataSetChanged()
+                        Toast.makeText(activity, "추가되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    //edit
+                    1 -> {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            data[dataPosition] = pill
+                            categoryArr[dataPosition] = pill
+                            selectCategoryData.clear()
+                            selectCategoryData[selectCategory] = categoryArr
+                            //후
+                            //println(categoryArr[dataPosition])
+                        }
+                        println("선택 $selectCategory")
+                        //livedata
+                        sharedViewModel!!.categoryLiveData.value = selectCategoryData
+                        println(testselectCategoryData)
+                        homeAdapter!!.notifyDataSetChanged()
+                        //Toast.makeText(activity, "수정되었습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, "$testselectCategoryData", Toast.LENGTH_SHORT).show()
+                    }
+                }*/
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (System.currentTimeMillis() - backPressedTime < 2500) {
+                    activity?.finish()
+                    return
+                }
+                Toast.makeText(activity, "한 번 더 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+                backPressedTime = System.currentTimeMillis()
+            }
+        }
+        activity?.onBackPressedDispatcher!!.addCallback(this, callback)
+        //mainActivity = context as MainActivity
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TaxiTabFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TaxiTabFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}

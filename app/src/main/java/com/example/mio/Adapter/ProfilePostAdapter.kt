e1e59ef1116f3814e7ee19522426ac0deb017c13
kkh1486 <102204603+kkh1486@users.noreply.github.com>
2023-11-15 02:44:29 +0900

    하던거

---
+++ app/src/main/java/com/example/mio/Adapter/ProfilePostAdapter.kt
@@ -0,0 +1,117 @@
+package com.example.mio.Adapter
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mio.Model.PostData
+import com.example.mio.R
+import com.example.mio.databinding.PostItemBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
+import java.lang.ref.WeakReference
+
+
+class ProfilePostAdapter : RecyclerView.Adapter<ProfilePostAdapter.ProfilePostViewHolder>(){
+    private lateinit var binding : PostItemBinding
+    var profilePostItemData = ArrayList<PostData>()
+    private lateinit var context : Context
+
+    init {
+        setHasStableIds(true)
+    }
+
+    inner class ProfilePostViewHolder(private val binding : PostItemBinding ) : RecyclerView.ViewHolder(binding.root) {
+        private var position : Int? = null
+        //var accountId = binding.accountId
+        //var accountProfile = binding.accountImage
+        var postTitle = binding.postTitle
+        var postDate = binding.postDate
+        var postLocation = binding.postLocation
+        var postParticipation = binding.postParticipation
+        var postParticipantTotal = binding.postParticipationTotal
+        var postCost = binding.postCost
+
+        fun bind(accountData: PostData, position : Int) {
+            this.position = position
+            //accountId.text = accountData.accountID
+            val s = context.getString(R.string.setText, accountData.postTargetDate, accountData.postTargetTime)
+            postTitle.text = accountData.postTitle
+            postDate.text = s
+            postLocation.text = accountData.postLocation
+            postParticipation.text = accountData.postParticipation.toString()
+            postParticipantTotal.text = accountData.postParticipationTotal.toString()
+            postCost.text = context.getString(R.string.setCost, accountData.postCost.toString())
+
+            //accountProfile.setImageURI() = pillData.pillTakeTime
+            //val listener = itemClickListener?.get()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfilePostViewHolder {
+        context = parent.context
+        binding = PostItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProfilePostViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProfilePostViewHolder, position: Int) {
+        holder.bind(profilePostItemData[holder.adapterPosition], position)
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, holder.adapterPosition, profilePostItemData[holder.adapterPosition].postID)
+        }
+
+        /*binding.homeRemoveIv.setOnClickListener {
+                val builder : AlertDialog.Builder = AlertDialog.Builder(context)
+                val ad : AlertDialog = builder.create()
+                var deleteData = pillItemData[holder.adapterPosition]!!.pillName
+                builder.setTitle(deleteData)
+                builder.setMessage("정말로 삭제하시겠습니까?")
+                builder.setNegativeButton("예",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        ad.dismiss()
+                        //temp = listData[holder.adapterPosition]!!
+                        //extraditeData()
+                        //testData.add(temp)
+                        //deleteServerData = tempServerData[holder.adapterPosition]!!.api_id
+                        removeData(holder.adapterPosition)
+                        //removeServerData(deleteServerData!!)
+                        //println(deleteServerData)
+                    })
+
+                builder.setPositiveButton("아니오",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        ad.dismiss()
+                    })
+                builder.show()
+            }*/
+    }
+
+    override fun getItemCount(): Int {
+        return profilePostItemData.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    //데이터 Handle 함수
+    fun removeData(position: Int) {
+        profilePostItemData.removeAt(position)
+        //temp = null
+        notifyItemRemoved(position)
+    }
+
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int, itemId: Int)
+    }
+
+    //약한 참조로 참조하는 객체가 사용되지 않을 경우 가비지 콜렉션에 의해 자동해제
+    //private var itemClickListener: WeakReference<ItemClickListener>? = null
+    private lateinit var itemClickListener: ProfilePostAdapter.ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ProfilePostAdapter.ItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+
+}
---
+++ app/src/main/java/com/example/mio/TabAccount/ProfilePostFragment.kt
@@ -0,0 +1,307 @@
+package com.example.mio.TabAccount
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mio.*
+import com.example.mio.Adapter.MyAccountPostAdapter
+import com.example.mio.Adapter.ProfilePostAdapter
+import com.example.mio.Model.PostData
+import com.example.mio.Model.PostReadAllResponse
+import com.example.mio.NoticeBoard.NoticeBoardReadActivity
+import com.example.mio.databinding.FragmentProfilePostBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfilePostFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfilePostFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    private lateinit var ppBinding : FragmentProfilePostBinding
+    private var myAdapter : ProfilePostAdapter? = null
+    private var profilePostAllData = ArrayList<PostData>()
+    private var manager : LinearLayoutManager = LinearLayoutManager(activity)
+
+    private var dataPosition = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        ppBinding = FragmentProfilePostBinding.inflate(inflater, container, false)
+
+        initMyRecyclerView()
+        initSwipeRefresh()
+
+        myAdapter!!.setItemClickListener(object : ProfilePostAdapter.ItemClickListener {
+            override fun onClick(view: View, position: Int, itemId: Int) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val temp = profilePostAllData[position]
+                    dataPosition = position
+                    val intent = Intent(activity, NoticeBoardReadActivity::class.java).apply {
+                        putExtra("type", "READ")
+                        putExtra("postItem", temp)
+                        putExtra("uri", temp!!.user.profileImageUrl)
+                    }
+                    startActivity(intent)
+                }
+            }
+        })
+
+
+        return ppBinding.root
+    }
+
+    private fun initSwipeRefresh() {
+        ppBinding.profileSwipe.setOnRefreshListener {
+            //새로고침 시 터치불가능하도록
+            activity?.window!!.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE) // 화면 터치 못하게 하기
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                setMyPostData()
+                myAdapter!!.profilePostItemData = profilePostAllData
+                //noticeBoardAdapter.recyclerView.startLayoutAnimation()
+                ppBinding.profileSwipe.isRefreshing = false
+                myAdapter!!.notifyDataSetChanged()
+                activity?.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            }, 1000)
+            //터치불가능 해제ss
+            //activity?.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            activity?.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        }
+    }
+
+
+    private fun setMyPostData() {
+        val saveSharedPreferenceGoogleLogin = SaveSharedPreferenceGoogleLogin()
+        val token = saveSharedPreferenceGoogleLogin.getToken(activity).toString()
+        val getExpireDate = saveSharedPreferenceGoogleLogin.getExpireDate(activity).toString()
+        val email = saveSharedPreferenceGoogleLogin.getUserEMAIL(activity)!!.substring(0 until 8)
+        val profileUserId = saveSharedPreferenceGoogleLogin.getProfileUserId(activity)!!
+
+        val interceptor = Interceptor { chain ->
+            var newRequest: Request
+            if (token != null && token != "") { // 토큰이 없는 경우
+                // Authorization 헤더에 토큰 추가
+                newRequest =
+                    chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
+                val expireDate: Long = getExpireDate.toLong()
+                if (expireDate <= System.currentTimeMillis()) { // 토큰 만료 여부 체크
+                    //refresh 들어갈 곳
+                    newRequest =
+                        chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
+                    return@Interceptor chain.proceed(newRequest)
+                }
+            } else newRequest = chain.request()
+            chain.proceed(newRequest)
+        }
+        val SERVER_URL = BuildConfig.server_URL
+        val retrofit = Retrofit.Builder().baseUrl(SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+        val builder = OkHttpClient.Builder()
+        builder.interceptors().add(interceptor)
+        val client: OkHttpClient = builder.build()
+        retrofit.client(client)
+        val retrofit2: Retrofit = retrofit.build()
+        val api = retrofit2.create(MioInterface::class.java)
+        ///////////////////////////////////////////////////
+        val call = RetrofitServerConnect.service
+
+        CoroutineScope(Dispatchers.IO).launch {
+            call.getMyPostData(profileUserId,"createDate,desc", 0, 5).enqueue(object : Callback<PostReadAllResponse> {
+                override fun onResponse(call: Call<PostReadAllResponse>, response: Response<PostReadAllResponse>) {
+                    if (response.isSuccessful) {
+
+                        //데이터 청소
+                        profilePostAllData.clear()
+
+                        for (i in response.body()!!.content.indices) {
+                            //탑승자 null체크
+                            var part = 0
+                            var location = ""
+                            var title = ""
+                            var content = ""
+                            var targetDate = ""
+                            var targetTime = ""
+                            var categoryName = ""
+                            var cost = 0
+                            var verifyGoReturn = false
+                            if (response.isSuccessful) {
+                                part = try {
+                                    response.body()!!.content[i].participants!!.isEmpty()
+                                    response.body()!!.content[i].participants!!.size
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    0
+                                }
+                                location = try {
+                                    response.body()!!.content[i].location.isEmpty()
+                                    response.body()!!.content[i].location
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "수락산역 3번 출구"
+                                }
+                                title = try {
+                                    response.body()!!.content[i].title.isEmpty()
+                                    response.body()!!.content[i].title
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "null"
+                                }
+                                content = try {
+                                    response.body()!!.content[i].content.isEmpty()
+                                    response.body()!!.content[i].content
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "null"
+                                }
+                                targetDate = try {
+                                    response.body()!!.content[i].targetDate.isEmpty()
+                                    response.body()!!.content[i].targetDate
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "null"
+                                }
+                                targetTime = try {
+                                    response.body()!!.content[i].targetTime.isEmpty()
+                                    response.body()!!.content[i].targetTime
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "null"
+                                }
+                                categoryName = try {
+                                    response.body()!!.content[i].category.categoryName.isEmpty()
+                                    response.body()!!.content[i].category.categoryName
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    "null"
+                                }
+                                cost = try {
+                                    response.body()!!.content[i].cost
+                                    response.body()!!.content[i].cost
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    0
+                                }
+                                verifyGoReturn = try {
+                                    response.body()!!.content[i].verifyGoReturn
+                                } catch (e : java.lang.NullPointerException) {
+                                    Log.d("null", e.toString())
+                                    false
+                                }
+                            }
+
+                            //println(response!!.body()!!.content[i].user.studentId)
+                            profilePostAllData.add(
+                                PostData(
+                                    response.body()!!.content[i].user.studentId,
+                                    response.body()!!.content[i].postId,
+                                    title,
+                                    content,
+                                    targetDate,
+                                    targetTime,
+                                    categoryName,
+                                    location,
+                                    //participantscount가 현재 참여하는 인원들
+                                    part,
+                                    //numberOfPassengers은 총 탑승자 수
+                                    response.body()!!.content[i].numberOfPassengers,
+                                    cost,
+                                    verifyGoReturn,
+                                    response.body()!!.content[i].user
+                                ))
+
+                            myAdapter!!.notifyDataSetChanged()
+                        }
+                        if (profilePostAllData.size > 0) {
+                            ppBinding.profilePostNotDataLl.visibility = View.GONE
+                            ppBinding.profileSwipe.visibility = View.VISIBLE
+                            ppBinding.profilePostRv.visibility = View.VISIBLE
+                        } else {
+                            ppBinding.profilePostNotDataLl.visibility = View.VISIBLE
+                            ppBinding.profileSwipe.visibility = View.GONE
+                            ppBinding.profilePostRv.visibility = View.GONE
+                        }
+
+                    } else {
+                        Log.d("f", response.code().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<PostReadAllResponse>, t: Throwable) {
+                    Log.d("error", t.toString())
+                }
+            })
+        }
+    }
+
+    private fun initMyRecyclerView() {
+        setMyPostData()
+        myAdapter = ProfilePostAdapter()
+        myAdapter!!.profilePostItemData = profilePostAllData
+        ppBinding.profilePostRv.adapter = myAdapter
+        //레이아웃 뒤집기 안씀
+        //manager.reverseLayout = true
+        //manager.stackFromEnd = true
+        ppBinding.profilePostRv.setHasFixedSize(true)
+        ppBinding.profilePostRv.layoutManager = manager
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ProfliePostFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ProfilePostFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
